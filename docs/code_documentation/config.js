0c57891cfb5f9fb1cd1eae7d56b2d7446b0e7824
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2023-08-28 15:58:29 +0200

    nlcs documentatie informatiemodel opstellen

--- docs/code_documentation/config.js
+++ docs/code_documentation/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   // => https://github.com/stichting-crow/respec/wiki
   specStatus: "DRAFT",
-  shortName: "nlcs/code_documentation",
+  shortName: "NLCS/code_documentation",
   specType: "technische documentatie", // vrije tekst, kleine letter
   imprint: "bimloket",
   editors: [
@@ -22,36 +22,6 @@ var respecConfig = {
   license: "cc-by",
   lint: { "no-unused-dfns": false },
   localBiblio: {
-    LeidraadSE3: {
-      title: "Leidraad SE versie 3",
-      href: "https://www.leidraadse.nl/assets/files/downloads/LeidraadSE/V3/Leidraad_V3_SE_web.pdf",
-      status: "Actueel",
-      publisher: "Leidraad SE",
-  },
-    LeidraadSE2: {
-    title: "Leidraad SE versie 2",
-    href: "https://www.leidraadse.nl/assets/files/downloads/LeidraadSE/V2/LeidraadSE_def_lowres.pdf",
-    status: "Vervallen",
-    publisher: "Leidraad SE",
-  },
-  NEN_EN_ISO_9000_2015: {
-    title: "NEN-EN-ISO 9000:2015 Kwaliteitsmanagementsystemen - Grondbeginselen en verklarende woordenlijst",
-    href: "https://www.nen.nl/en/nen-en-iso-9000-2015-en-nl-211925",
-    status: "Actueel",
-    publisher: "NEN",
-  },
-  ISO_IEC_IEEE_29148_2018: {
-    title: "ISO/IEC/IEEE 29148:2018 Systems and software engineering — Life cycle processes — Requirements engineering",
-    href: "https://www.iso.org/standard/72089.html",
-    status: "Actueel",
-    publisher: "ISO",
-  },
-  ISO_IEC_IEEE_15288_2015: {
-    title: "ISO/IEC/IEEE 15288 Systems and software engineering - System life cycle processes",
-    href: "https://www.iso.org/standard/63711.html",
-    status: "Actueel",
-    publisher: "ISO",
-  },
   NEN_2660_2_2022: {
     title: "NEN 2660-2 Regels voor informatiemodellering van de gebouwde omgeving - Deel 2: Praktische configuratie, extensie en implementatie van NEN 2660-1",
     href: "https://www.nen.nl/en/nen-2660-2-2022-nl-291667",
--- docs/code_documentation/h/0-samenvatting.md
+++ docs/code_documentation/h/0-samenvatting.md
@@ -1,1 +1,5 @@
 Dit document beschrijft het informatiemodel van NLCS. NLCS is als linked data gepubliceerd. 
+
+<p><dfn>NLCS</dfn> Een afkorting van Nederlandse CAD Standaard, een standaard voor het maken en overdragen van digitale [=CAD-modellen=] in de openbare ruimte en infrastructuur.</p>
+
+<p><dfn>CAD</dfn> "Computer Aided Design" ofwel het ontwerpen van ruimtelijke objecten met behulp van een applicatie.</p>
