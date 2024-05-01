ed75206b8b51de93b473702415722baa23a2be8f
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2024-05-01 22:12:45 +0200

    update-requirementssoftware

--- docs/requirementssoftware/config.js
+++ docs/requirementssoftware/config.js
@@ -1,9 +1,11 @@
 var respecConfig = {
   // => https://github.com/stichting-crow/respec/wiki
-  specStatus: "DRAFT",
+  specStatus: "CR",
+  reviewDateEnd: "2024-09-01",
   shortName: "NLCS/requirementssoftware",
   specType: "technische documentatie", // vrije tekst, kleine letter
   imprint: "digigo",
+  hypothesisComments: "true",
   editors: [
     // alleen `name` is verplicht, https://respec.org/docs/#person
     {
